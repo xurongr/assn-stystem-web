85c2ca572ce00da7dfcc765396f7f5f96ca6070e
xrr <1004956347@qq.com>
2019-02-26 17:32:34 +0800

    用户管理页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import manageIndex from "@/views/backstageManage/index";
+import manageIndex from "@/views/backstageManage";
+import userManage from "@/views/backstageManage/userManage";
 
 Vue.use(Router)
 
@@ -10,15 +11,25 @@ export default new Router({
     {
       path: '/',
       redirect: 'manageIndex',
-      component:manageIndex
     },
     {
       path: '/manageIndex',
       name: 'manageIndex',
       component:manageIndex,
-      meta: {
-        title: '后台管理首页'
-      }
+      children: [
+        {
+          path:"",
+          title:"用户管理",
+          name:"userManage",
+          component:userManage
+        },
+        {
+          path: 'userManage',
+          title: '用户管理',
+          name: 'userManage',
+          component:userManage,
+        }
+      ]
     }
   ]
 })
